35c7602997e7209850cde4ea2ddf1ba13d4863dd
TsaoLun <ycl18@live.com>
2020-11-08 14:50:39 +0800

    update

--- nodelib/node2.js
+++ nodelib/node2.js
@@ -25,7 +25,7 @@ process.stdin.on('readable', function () {
 }); */
 
 
-//---缓冲器 buffer 与类型化数组 Uint8Array---
+/* //---缓冲器 buffer 与类型化数组 Uint8Array---
 
 let a = [1, 2, 3];
 let b = Buffer.from(a);
@@ -38,3 +38,67 @@ console.log(b2);
 
 let b3 = Buffer.alloc(10);
 console.log(b3);
+
+let b4 = Buffer.allocUnsafe(10);
+console.log(b4); 
+
+//Buffer => JSON
+
+let buf = Buffer.from('This is my pretty example');
+let json = JSON.stringify(buf);
+
+console.log(json);
+
+//JSON => Buffer => String
+let buf2 = Buffer.from(JSON.parse(json).data);
+console.log(buf2.toString());
+
+//StringDecoder 获取完整 UTF-8 字符序列
+let StringDecoder = require('string_decoder').StringDecoder;
+let decoder = new StringDecoder('utf8');
+
+let euro = new Buffer.from([0xE2, 0x82]);
+let euro2 = new Buffer.from([0xAC]);
+
+console.log(decoder.write(euro));
+console.log(decoder.write(euro2));
+
+console.log(euro.toString());
+console.log(euro2.toString());
+
+//字符串写入缓冲器
+let buf = Buffer.alloc(3);
+buf.write('$', 'utf-8');
+console.log(buf.toString());
+console.log(buf.length);
+
+//缓冲器操作
+var buf = new Buffer.alloc(4);
+
+buf.writeUInt8(0x63, 0);
+buf.writeUInt8(0x61, 1);
+buf[3] = 0x73;
+buf[2] = 0x74;
+
+console.log(buf.toString());
+
+//buffer.slice() 创建新缓冲器及修改
+var buf1 = new Buffer.from('this is the way we build our buffer');
+var lnth = buf1.length;
+
+console.log(buf1.slice().toString());
+
+var buf2 = buf1.slice(19, lnth);
+console.log(buf2.toString());
+
+buf2.fill('*', 0, 5);
+console.log(buf2.toString());
+console.log(buf1.toString());
+
+console.log(buf1.equals(buf2));
+//buffer.copy() 从一个缓冲器复制到另一个(略)
+//buf1.compare(buf2) 前大于后返回 1 否则 -1*/
+
+//---Node 回调函数和异步事件处理---
+
+
