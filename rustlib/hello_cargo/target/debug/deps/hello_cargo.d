c52ec35b3795d9e96e09fcd06c5af6c8945da6c4
TsaoLun <ycl18@live.com>
2020-11-12 23:28:21 +0800

    o\

---
+++ rustlib/hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+/Users/lun/Projects/Notes/rustlib/hello_cargo/target/debug/deps/hello_cargo: src/main.rs
+
+/Users/lun/Projects/Notes/rustlib/hello_cargo/target/debug/deps/hello_cargo.d: src/main.rs
+
+src/main.rs:
---
+++ rustlib/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/lun/Projects/Notes/rustlib/hello_cargo/target/debug/hello_cargo: /Users/lun/Projects/Notes/rustlib/hello_cargo/src/main.rs
