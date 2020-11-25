ee32ea53f4e6a8974e8ed78bb7ec9dc70a8de6ab
TsaoLun <ycl18@live.com>
2020-11-26 00:35:08 +0800

    原型模式

--- jslib/pro8.js
+++ jslib/pro8.js
@@ -108,7 +108,7 @@ console.log(dest !== src);
 console.log(result);
 console.log(dest);*/
 
-//工厂模式创建对象
+/* //工厂模式创建对象
 function createPerson(name, age, job) {
     let o = new Object();
     o.name = name;
@@ -119,9 +119,93 @@ function createPerson(name, age, job) {
     };
     return o;
 }
-let person1 = createPerson("Nicholas", 29, "Software Engineer");
-let person2 = createPerson("Greg", 27, "Doctor");
-console.log(person1.name);
-console.log(person2.sayName());
+// let person1 = createPerson("Nicholas", 29, "Software Engineer");
+// let person2 = createPerson("Greg", 27, "Doctor");
 
-//构造函数模式
+//构造函数模式(能创建对象的函数)
+function Person(name, age, job) {
+    this.name = name;
+    this.age = age;
+    this.job = job;
+    this.sayName = function () {
+        console.log(this.name);
+    };
+}
+let person1 = new Person("Nicholas", 29, "Software Engineer"); 
+let person2 = new Person("Greg", 27, "Doctor");
+
+person1.sayName();
+person2.sayName();
+
+console.log(person1.constructor === Person); //true
+console.log(Person instanceof Function); //true
+console.log(person1 instanceof Function); //false
+console.log(person1 instanceof Object); //true
+console.log(person1 instanceof Person); //true 
+
+//不需要传参可省略括号
+function Person() {
+    this.name = "Jake";
+    this.sayName = function () {
+        console.log(this.name);
+    };
+}
+let person1 = new Person();
+let person2 = new Person;
+
+person1.sayName();
+person2.sayName();
+
+let n = 1;
+function sayName() {
+    n++;
+    console.log('yes');
+}
+sayName;
+sayName();
+console.log(n);
+
+//不使用 new 则添加至 window/global
+function Person(name, age, job) {
+    this.name = name;
+    this.age = age;
+    this.job = job;
+    this.sayName = function () {
+        console.log(this.name);
+    };
+}
+Person("Greg", 27, "Doctor");
+console.log(name);
+console.log(global.name);
+global.sayName();*/
+
+//构造函数的 Function 重复创建
+function Person(name, age, job) {
+    this.name = name;
+    this.age = age;
+    this.job = job;
+    this.sayName = function () {
+        console.log(this.name);
+    };
+}
+let person1 = new Person("Nicholas", 29, "Software Engineer"); 
+let person2 = new Person("Greg", 27, "Doctor");
+console.log(person1.sayName == person2.sayName);
+//问题：函数体每次会 new 一个新的 Function
+//解决方法：将函数定义转移到构造函数外部
+function PersonV2(name, age, job) {
+    this.name = name;
+    this.age = age;
+    this.job = job;
+    this.sayName = sayName;
+}
+function sayName(x) {
+    console.log(`${x}! `+this.name);
+}
+let personV1 = new PersonV2("Nicholas", 29, "Software Engineer"); 
+let personV2 = new PersonV2("Greg", 27, "Doctor");
+personV1.sayName(); //undefined! Nicholas
+personV1.sayName; // nothing
+console.log(personV1.sayName == personV2.sayName);
+
+//原型模式
