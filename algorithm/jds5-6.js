8d5b56b676b9a0e455b435de10fe2e32c84260c5
TsaoLun <ycl18@live.com>
2021-02-08 22:41:28 +0800

    update

--- algorithm/jds5-6.js
+++ algorithm/jds5-6.js
@@ -163,7 +163,7 @@ console.log(deque.size());
 console.log(deque.isEmpty()); */
 
 //--- 击鼓传花 ---
-function hotPatoto(elementList, num) {
+/* function hotPatoto(elementList, num) {
     const queue = new Queue();
     const eliminatedList = [];
 
@@ -190,6 +190,36 @@ const result = hotPatoto(names, 7);
 result.eliminated.forEach(name => {
     console.log(`${name}在击鼓传花中被淘汰`);
 });
-console.log(`胜利者：${result.winner}`);
+console.log(`胜利者：${result.winner}`); */
 
 //--- 回文检查器 ---
+function palindromeChecker(aString) {
+    if (aString === undefined || aString === null ||
+        (aString !== null && aString.length === 0)) {
+        return false;
+    }
+    const deque = new Deque();
+    const lowerString = aString.toLocaleLowerCase().split(' ').join('');
+    //split(' ').join('') 将字符串中的空格剔除
+    let isEqual = true;
+    let firstChar, lastChar;
+
+    for (let i = 0; i < lowerString.length; i++) {
+        deque.addBack(lowerString.charAt(i));
+        //string.charAt(index) 或 string[]
+    }
+
+    while (deque.size() > 1 && isEqual) {
+        firstChar = deque.removeFront();
+        lastChar = deque.removeBack();
+        if (firstChar !== lastChar){
+            isEqual = false;
+        }
+    }
+
+    return isEqual;
+}
+console.log('a',palindromeChecker('a'));
+console.log('aa',palindromeChecker('aa'));
+console.log('qaa q',palindromeChecker('qaa q'));
+
